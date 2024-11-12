d094ce3a3eeeb42097c6e14dd37d25389bd40e8e
Breno Gonzaga <contato@brenogonzaga.com>
2024-11-11 21:02:11 -0400

    chore: update deps

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,14 @@
-export { auth as middleware } from "./auth";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  console.warn("ROUTE: ", req.url);
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
