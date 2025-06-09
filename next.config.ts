6991efdbb0012e979af2a60b4d755332e019c11c
Breno Gonzaga <contato@brenogonzaga.com>
2025-06-08 23:42:20 -0400

    change to use open next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["@prisma/client", ".prisma/client"],
 };
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
